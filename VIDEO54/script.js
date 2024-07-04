5579e927aae9ebef8a5969277ee6fbe4a8bc8c0a
Aryan1943 <122879041+Aryan1943@users.noreply.github.com>
2024-07-04 23:10:38 +0530

    Add files via upload

---
+++ VIDEO54/script.js
@@ -0,0 +1,15 @@
+alert("hello world");
+console.log("hey i am console")
+var a=prompt("enter your number");
+console.log("your number is "+a)
+var isTrue=prompt("are you sure blasting your computer");
+
+if(isTrue)
+    {
+        console.log("computer is blasting");
+    }
+    else{
+        console.log("your number is " + a);
+    }
+    document.title="hey am good"
+    document.body.style.backgroundColor="red"
---
+++ Video55/script.js
@@ -0,0 +1,15 @@
+console.log("hey this is tutorial 55")
+let a =5;
+let b=6;             // integer
+console.log(a+b+8)
+let c="harry"       //String
+console.log(typeof a,typeof b,typeof c);
+
+var _a="shubham";
+//var 55a="rohan"  // not allowed
+
+console.log(a+b+8);
+console.log(typeof a, typeof b,typeof c);
+
+// const a1=6;
+// a1=a1+1  not allowed because a1 is constant
